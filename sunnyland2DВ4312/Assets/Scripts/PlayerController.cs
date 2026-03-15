583eec552118f94c206145abb31a8a79c919247b
AndrewBezotchestvo <96548028+AndrewBezotchestvo@users.noreply.github.com>
2026-03-15 18:05:49 +0800

    add

---
+++ sunnyland2DВ4312/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float HP = 100;
+    public float MaxHP = 100;
+    public float Score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- sunnyland2DВ4312/Assets/Scripts/PlayerMove.cs
+++ sunnyland2DВ4312/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,48 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 5;
     public float jumpForce = 10;
+    public float rayLenght = 1;
+    public Vector2 rayOffset = Vector2.zero;
 
-    private bool isGround = false;
+    private bool isGround = true;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        animator.SetBool("IsJump", !isGround);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGround == true)
+        {
+            movement.y = jumpForce;
+            rb.linearVelocity = movement;
+            //rb.linearVelocityY = jumpForce;
+            //rb.AddForce(jumpForce * transform.up);
+        }
+
+RaycastHit2D ground = Physics2D.Raycast((Vector2)transform.position + rayOffset, Vector2.down, rayLenght);
+        isGround = ground.collider != null;
+
+
+        if (rb.linearVelocityX > 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        else if (rb.linearVelocityX < 0)
+        {
+            transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+
+    }
+
 
     void FixedUpdate()
     {
